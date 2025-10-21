6993bdc41c2fbaa9f6267a8652d901bda94a0702
Ibrahim Khalil <78839403+ibrahimkhalil92@users.noreply.github.com>
2025-10-21 17:22:23 +0500

    add contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ const HomeSection2 = dynamic(() => import("@/components/home/Section2"));
 const HomeSection3 = dynamic(() => import("@/components/home/Section3"));
 const HomeSection4 = dynamic(() => import("@/components/home/Section4"));
 const HomeSection5 = dynamic(() => import("@/components/home/Section5"));
+const HomeSection6 = dynamic(() => import("@/components/home/Section6"));
+
 const Home = () => {
   return (
     <PageBox>
@@ -14,6 +16,7 @@ const Home = () => {
       <HomeSection3 id="experiences" />
       <HomeSection4 id="skills" />
       <HomeSection5 id="projects" />
+      <HomeSection6 id="contact" />
     </PageBox>
   );
 };

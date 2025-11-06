20a09f413881d6464f169170e04e64ecc0b534b3
Ibrahim Khalil <78839403+ibrahimkhalil92@users.noreply.github.com>
2025-11-06 15:56:18 +0500

    add weather-app in projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,10 +9,21 @@ const projects: IProjectItem[] = [
     icon: "/skills/nextjs.png",
     repoType: RepoType.Public,
     projectType: ProjectType.Personal,
-    githubUrl: "https://github.com/ibrhm42/portfolio",
+    githubUrl: "https://github.com/ibrhm42/portfolio-nextjs",
     url: "https://ibrahimkhalil.dev",
-    tags: ["Next.js", "Sass", "Web Development"],
+    tags: ["Next.js", "Sass", "Web Development", "Tailwind"],
   },
-
+  {
+    id: "weather-app",
+    title: "Weather App",
+    description:
+      "This repository contains the source code for a Weather App built using Next.js and Tailwind.",
+    icon: "/skills/nextjs.png",
+    repoType: RepoType.Public,
+    projectType: ProjectType.Personal,
+    githubUrl: "https://github.com/ibrhm42/weather-app-nextjs",
+    url: "https://ibrahimkhalil.dev",
+    tags: ["Next.js", "Sass", "Web Development", "Tailwind"],
+  },
 ];
 export default projects;

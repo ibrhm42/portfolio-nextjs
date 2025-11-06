5dab24ed8d059258f4f5a8f1b10750b6bc6a9365
Ibrahim Khalil <78839403+ibrahimkhalil92@users.noreply.github.com>
2025-11-06 22:19:50 +0500

    add weather app built on react in projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,6 +2,30 @@ import { IProjectItem, ProjectType, RepoType } from "@/types";
 
 const projects: IProjectItem[] = [
   {
+    id: "weather-app",
+    title: "Weather App",
+    description:
+      "This repository contains the source code for a weather app built using Next.js and Tailwind.",
+    icon: "/skills/nextjs.png",
+    repoType: RepoType.Public,
+    projectType: ProjectType.Personal,
+    githubUrl: "https://github.com/ibrhm42/weather-app-nextjs",
+    url: "https://weathr-app-nextjs.vercel.app",
+    tags: ["Next.js", "Sass", "Web Development", "Tailwind"],
+  },
+  {
+    id: "movies-app",
+    title: "Movies App",
+    description:
+      "This repository contains the source code for a movies app built using Next.js and Tailwind.",
+    icon: "/skills/react.svg",
+    repoType: RepoType.Public,
+    projectType: ProjectType.Personal,
+    githubUrl: "https://github.com/ibrhm42/imdb",
+    url: "https://ibrhm42.github.io/imdb",
+    tags: ["React", "Sass", "Web Development"],
+  },
+  {
     id: "portfolio-website",
     title: "Portfolio Website",
     description:
@@ -14,27 +38,15 @@ const projects: IProjectItem[] = [
     tags: ["Next.js", "Sass", "Web Development", "Tailwind"],
   },
   {
-    id: "weather-app",
-    title: "Weather App",
+    id: "weather-app-react",
+    title: "Weather App - React",
     description:
-      "This repository contains the source code for a weather app built using Next.js and Tailwind.",
-    icon: "/skills/nextjs.png",
-    repoType: RepoType.Public,
-    projectType: ProjectType.Personal,
-    githubUrl: "https://github.com/ibrhm42/weather-app-nextjs",
-    url: "https://weathr-app-nextjs.vercel.app",
-    tags: ["Next.js", "Sass", "Web Development", "Tailwind"],
-  },
-    {
-    id: "movies-app",
-    title: "Movies App",
-    description:
-      "This repository contains the source code for a movies app built using Next.js and Tailwind.",
+      "This repository contains the source code for a weather app built using Next.js and Sass.",
     icon: "/skills/react.svg",
     repoType: RepoType.Public,
     projectType: ProjectType.Personal,
-    githubUrl: "https://github.com/ibrhm42/imdb",
-    url: "https://ibrhm42.github.io/imdb",
+    githubUrl: "https://github.com/ibrhm42/weather-app",
+    url: "https://ibrhm42.github.io/weather-app",
     tags: ["React", "Sass", "Web Development"],
   },
 ];
